9f3cbc056c2d62bc7aa95177af35cb242e4804c2
AnanaGame <gameanana@gmail.com>
2018-09-12 22:13:06 +0200

    Write a little documentation on the API

--- src/views/Api.vue
+++ src/views/Api.vue
@@ -10,6 +10,16 @@
         </div>
       </div>
     </div>
+    <div class="container">
+      <h2>API</h2>
+      <p>La REST Api d'Olivia est disponible en libre service à cette adresse <strong>https://olivia-api.herokuapp.com/</strong></p>
+      <h2>POST /api/response</h2>
+      <p>
+        Pour obtenir une réponse d'olivia vous devez envoyer une requête POST sur /api/response avec ces paramètres dans le <strong>body</strong> :
+      </p>
+      <p>- <strong>sentence</strong>, en valeur la phrase à envoyer à Olivia</p>
+      <p>- <strong>authorId</strong>, en valeur l'identifiant de l'auteur de la phrase</p>
+    </div>
   </div>
 </template>
 
