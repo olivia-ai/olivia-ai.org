78a606d94d251aed17a75a43991b9ab5042a6589
AnanaGame <gameanana@gmail.com>
2018-10-30 08:10:03 +0100

    Move install tip for ios

--- src/App.vue
+++ src/App.vue
@@ -5,18 +5,6 @@
 </template>
 
 <script>
-  const isIos = () => {
-    const userAgent = window.navigator.userAgent.toLowerCase();
-    return /iphone|ipad|ipod/.test(userAgent);
-  }
-  // Detects if device is in standalone mode
-  const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone);
-
-  // Checks if should display install popup notification:
-  if (isIos() && !isInStandaloneMode()) {
-    this.setState({ showInstallMessage: true });
-  }
-
   export default {
     name: 'app'
   }
