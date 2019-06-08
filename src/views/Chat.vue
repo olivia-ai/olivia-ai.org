d7cbfbf9fff70ac40b23eb64a11120bb43d94fd7
AnanaGame <gameanana@gmail.com>
2019-06-08 09:21:14 +0200

    Add a useless statement

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -43,6 +43,7 @@
     methods: {
       speak(text) {
         let message = new SpeechSynthesisUtterance(text)
+        this.voice
         message.voice = this.voice
         message.lang = "en-US"
         window.speechSynthesis.speak(message);
