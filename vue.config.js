830612e7dca541aee56c3f4075712f5b1f3fe60f
AnanaGame <gameanana@gmail.com>
2019-07-02 10:28:47 +0200

    Update vue.config.js PWA properties

--- vue.config.js
+++ vue.config.js
@@ -8,18 +8,20 @@ module.exports = {
       .end()
   },
   pwa: {
-    workboxPluginMode: 'InjectManifest',
-    workboxOptions: {
-      swSrc: 'src/service-worker.js',
-    },
+    name: "Olivia",
     appleMobileWebAppCapable: 'yes',
-    themeColor: '#f32173',
+    appleMobileWebAppStatusBarStyle: 'black-translucent',
+    themeColor: '#ff3aaf',
     msTileColor: '#fff',
     iconPaths: {
       favicon32: "img/icons/favicon-32x32.png",
       favicon16: "img/icons/favicon-16x16.png",
       appleTouchIcon: "img/icons/apple-touch-icon-152x152.png",
       msTileImage: "img/icons/msapplication-icon-144x144.png"
+    },
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      swSrc: 'src/service-worker.js',
     }
   }
 }
