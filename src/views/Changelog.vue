bfbcd8f336b7330dbe25da04481faba94617baaa
AnanaGame <gameanana@gmail.com>
2019-08-08 13:35:06 +0200

    Releasing Olivia 2.0 in the changelog

--- src/views/Changelog.vue
+++ src/views/Changelog.vue
@@ -17,8 +17,15 @@
             <header class="timeline-header">
               <span class="tag is-medium is-primary">Latest</span>
             </header>
-            <div class="timeline-item">
-
+            <div class="timeline-item is-primary">
+              <div class="timeline-marker is-primary"></div>
+              <div class="timeline-content">
+                <p class="heading">August 2019</p>
+                <p class="content">
+                  <vue-markdown :source="august2019">
+                  </vue-markdown>
+                </p>
+              </div>
             </div>
             <header class="timeline-header">
               <span class="tag is-primary">2019</span>
@@ -47,3 +54,21 @@
     </div>
   </div>
 </template>
+
+<script>
+  import VueMarkdown from 'vue-markdown'
+
+  export default {
+    data() {
+      return {
+        august2019: 'Loading...'
+      }
+    },
+    components: {
+      VueMarkdown
+    },
+    mounted() {
+      import('../../public/changelog/august-2019.md').then(res => this.august2019 = res.default)
+    }
+  }
+</script>
