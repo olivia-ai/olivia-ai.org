ea64c77bf5de1c3c1ed9ca7afb7187214101d36d
Hugo Lageneste <hugo.lageneste@pm.me>
2021-10-18 18:40:53 +0200

    Create a page loader

---
+++ src/components/PageLoader.tsx
@@ -0,0 +1,39 @@
+import { Center, CircularProgress, Image } from '@chakra-ui/react'
+
+import React from 'react'
+
+const PageLoader = (): JSX.Element => (
+  <>
+    <style>{`
+    body {
+      overflow: hidden;
+    }
+    `}</style>
+
+    <Center 
+      w="100vw"
+      h="100vh"
+      position="fixed"
+      overflow="hidden"
+      backgroundColor="white"
+      zIndex="1000"
+    >
+      <Image 
+        src="olivia-character.svg" 
+        alt="Olivia character waving"
+        w={100}
+      />
+
+      <CircularProgress 
+        pos="absolute"
+        isIndeterminate 
+        color="olivia.500" 
+        size={200}
+        thickness="3px"
+        capIsRound
+      />  
+    </Center>
+  </>
+)
+
+export default PageLoader
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,31 @@
+import React, { useEffect, useState } from 'react'
+
 import Background from '../components/Background'
 import Footer from '../components/Footer'
 import Hero from '../components/heros/Heading'
 import NavigationBar from '../components/navigation-bar/Bar'
 import Network from '../components/heros/Network'
-import React from 'react'
+import PageLoader from '../components/PageLoader'
 
-const Index = (): JSX.Element => (
-  <>
+const Index = (): JSX.Element => {
+  const [ isLoading, setLoadingState ] = useState(true)
+
+  useEffect(() => {
+    // Show the loader for a minimum amount of time
+    setTimeout(() => {
+      setLoadingState(false)
+    }, 400)
+  }, [])
+
+  return <>
+    { isLoading ? <PageLoader /> : null }
+
     <Background />
     <NavigationBar />
     <Hero />
     <Network />
     <Footer />
   </>
-)
+}
 
 export default Index
